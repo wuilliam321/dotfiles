57a7d08b45eeae3010afc06a100aaee4f4864260
Wuilliam Lacruz <236566+wuilliam321@users.noreply.github.com>
2024-08-02 11:26:06 -0300

    add watcher improvements

--- run-on-changes.sh
+++ run-on-changes.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# Comprueba que se ha proporcionado el directorio y el comando
 if [ $# -lt 3 ]; then
   echo "Usage: $0 <path-to-directory> <file-type> <command-to-execute>"
   exit 1
@@ -8,15 +7,15 @@ fi
 DIRECTORY=$1
 FILE_TYPE=$2
 COMMAND=$3
+LOCKFILE="/tmp/command_lock.tmp"
+echo "Listening for changes in $DIRECTORY with file type $FILE_TYPE"
 
-echo "listening for changes in $DIRECTORY with file type $FILE_TYPE"
-
-# Usa fswatch para monitorear los archivos `.go` y ejecutar un comando proporcionado
-# fswatch -o $DIRECTORY | grep "\.$FILE_TYPE$" | while read file
-# fswatch -o $DIRECTORY | grep "\.go$" | while read file
 fswatch -e ".*" -i "\\.$FILE_TYPE$" $DIRECTORY | while read file
 do
-  echo "Changed: $file\n runnnig $COMMAND"
-  $COMMAND
-  echo "done"
+  if [ ! -f $LOCKFILE ]; then
+    touch $LOCKFILE
+    clear
+    echo "$COMMAND"
+    ($COMMAND; rm $LOCKFILE) &
+  fi
 done
