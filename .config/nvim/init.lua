59605b835af8644e0e80254116095abe561b765f
Wuilliam Lacruz <wuilliam321@gmail.com>
2024-08-26 23:59:12 -0300

    fix deprecated calls

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -44,8 +44,8 @@ vim.opt.scrolloff = 10
 vim.opt.hlsearch = true
 
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
+vim.keymap.set('n', '[d', function() vim.diagnostic.jump({count = 1}) end, { desc = 'Go to previous [D]iagnostic message' })
+vim.keymap.set('n', ']d', function() vim.diagnostic.jump({count = -1}) end, { desc = 'Go to next [D]iagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
@@ -276,7 +276,10 @@ require('lazy').setup({
       capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
 
       local servers = {
+        tsserver = {},
+        eslint = {},
         jsonls = {},
+        pylsp = {},
         gopls = {
           cmd = { '/Users/wlacruz/go/bin/gopls' },
           settings = {
@@ -632,7 +635,7 @@ require('lazy').setup({
     build = ':TSUpdate',
     config = function()
       require('nvim-treesitter.configs').setup {
-        ensure_installed = { 'bash', 'json', 'lua', 'markdown', 'vim', 'vimdoc', 'go', 'sql', 'c' },
+        ensure_installed = { 'bash', 'json', 'lua', 'markdown', 'vim', 'vimdoc', 'go', 'sql', 'jsdoc', 'c' },
         -- Autoinstall languages that are not installed
         auto_install = true,
         highlight = { enable = true },
